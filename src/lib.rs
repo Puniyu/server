1a10fbab700a04a6aba803294aed5c7fb3595157
wuliya <3369906077@qq.com>
2025-09-24 21:25:56 +0800

    feat(middleware): 添加访问日志中间件
    - 新增 logger.rs 模块实现访问日志记录功能
    - 实现 AccessLog 中间件，记录请求方法、路径、状态码、耗时和客户端 IP

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod server;
-pub  mod middleware;
+mod middleware;
 mod logger;
 
 pub  use server::run_server;
--- src/server.rs
+++ src/server.rs
@@ -1,19 +1,24 @@
-use std::net::IpAddr;
-use actix_web::HttpServer;
-use crate::info;
+use crate::{info, middleware};
 use crate::logger::log_init;
+use actix_web::middleware::{NormalizePath, TrailingSlash};
+use actix_web::{App, HttpServer};
+use std::net::IpAddr;
+
 pub async fn run_server(host: Option<IpAddr>, port: Option<u16>) {
-    log_init();
-    let host = host.unwrap_or(IpAddr::V4([127, 0, 0, 1].into()));
-    let port = port.unwrap_or(33720);
-    let addr = format!("{}:{}", host, port);
-    info!("服务器在 {} 运行", addr);
-    HttpServer::new(|| {
-        actix_web::App::new().service(actix_web::web::resource("/").to(|| async { "Hello World!" }))
-    })
-        .bind(addr)
-        .unwrap()
-        .run()
-        .await
-        .unwrap();
+	log_init();
+	let host = host.unwrap_or(IpAddr::V4([127, 0, 0, 1].into()));
+	let port = port.unwrap_or(33720);
+	let addr = format!("{}:{}", host, port);
+	info!("服务器在 {} 运行", addr);
+	HttpServer::new(|| {
+		App::new()
+            .wrap(middleware::AccessLog)
+			.wrap(NormalizePath::new(TrailingSlash::Trim))
+			.service(actix_web::web::resource("/").to(|| async { "Hello World!" }))
+	})
+	.bind(addr)
+	.unwrap()
+	.run()
+	.await
+	.unwrap();
 }
